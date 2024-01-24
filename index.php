1c18125dc757b9a17240f9462bdda6b72c494481
junio <9179@htl.rennweg.at>
2024-01-24 10:31:07 +0100

    bissl umgebaut und html dazugegeben

--- index.php
+++ index.php
@@ -7,31 +7,45 @@ use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\PngWriter;
 
-$result = Builder::create()
-    ->writer(new PngWriter())
-    ->writerOptions([])
-    ->data('Custom QR code contents')
-    ->encoding(new Encoding('UTF-8'))
-    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
-    ->size(300)
-    ->margin(10)
-    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
-    ->logoPath(__DIR__.'/assets/symfony.png')
-    ->logoResizeToWidth(50)
-    ->logoPunchoutBackground(true)
-    ->labelText('This is the label')
-    ->labelFont(new NotoSans(20))
-    ->labelAlignment(LabelAlignment::Center)
-    ->validateResult(false)
-    ->build();
+
+if (isset($_GET["phonenum"])) {
+    $result = Builder::create()
+        ->writer(new PngWriter())
+        ->writerOptions([])
+        ->data('Custom QR code contents')
+        ->encoding(new Encoding('UTF-8'))
+        ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+        ->size(300)
+        ->margin(10)
+        ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+        ->logoPath(__DIR__.'/assets/symfony.png')
+        ->logoResizeToWidth(50)
+        ->logoPunchoutBackground(true)
+        ->labelText('This is the label')
+        ->labelFont(new NotoSans(20))
+        ->labelAlignment(LabelAlignment::Center)
+        ->validateResult(false)
+        ->build();
 
 
 // Directly output the QR code
-header('Content-Type: '.$result->getMimeType());
-echo $result->getString();
+    header('Content-Type: '.$result->getMimeType());
+    echo $result->getString();
+} else {
+    echo <<<FORMULAR
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>QRCode</title>
+</head>
+    <body>
+    <form>
+               <label>Phonenumber:</label>
+               <input name="phonenum" type="text" required>
+                    <button type="submit">Code generieren</button>
+            </form>
+</body>
+</html>
+FORMULAR;
 
-// Save it to a file
-$result->saveToFile(__DIR__.'/qrcode.png');
-
-// Generate a data URI to include image data inline (i.e. inside an <img> tag)
-$dataUri = $result->getDataUri();
+}
