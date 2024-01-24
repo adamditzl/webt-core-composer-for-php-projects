0f197911685ee65894c059769cecbabe01a43424
junio <9179@htl.rennweg.at>
2024-01-24 10:26:24 +0100

    mal qr code basic von github von dem typen kopiert. noch nicht angepasst oder verändert

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data('Custom QR code contents')
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->logoPath(__DIR__.'/assets/symfony.png')
+    ->logoResizeToWidth(50)
+    ->logoPunchoutBackground(true)
+    ->labelText('This is the label')
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+
+
+// Directly output the QR code
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
+
+// Save it to a file
+$result->saveToFile(__DIR__.'/qrcode.png');
+
+// Generate a data URI to include image data inline (i.e. inside an <img> tag)
+$dataUri = $result->getDataUri();
